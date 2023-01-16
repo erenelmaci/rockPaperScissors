8648c0c1570ce3c1e3355929bdf7400720867a36
Eren Elmacı <105083973+erenelmaci@users.noreply.github.com>
2023-01-16 23:48:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ select.addEventListener("click", (e) => {
 const pcArr = ["tas", "kagit", "makas"]; //pc nin seçenekleri
 
 function pc() {
-  pcRandom = "tas";
+  pcRandom = Math.floor(Math.random()*3)
 
   pcChoice.innerHTML = `<img src="./assets/${pcRandom}.png"></img>`;
   result();
